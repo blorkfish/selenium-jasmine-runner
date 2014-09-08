ba34b4cebf92795960b42c042ebada17502e25b6
blorkfish <blorkfish@gmail.com>
2014-09-08 22:00:16 +0800

    updated with TeamCity reporter

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,9 @@ using System.Threading;
 //using NUnit.Framework;
 using ITDM;
 using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using SeleniumJasmineRunner;
 
 namespace HDWA.SeleniumTestRunner
 {
@@ -16,24 +19,32 @@ namespace HDWA.SeleniumTestRunner
         public List<TestSuite> Suites = new List<TestSuite>();
         public int PassCount;
         public int FailCount;
+        private ITestReporter _testReporter;
+
+        public TestSuites(ITestReporter reporter)
+        {
+            _testReporter = reporter;
+        }
         
         public void WriteToConsole()
         {
             foreach (TestSuite suite in Suites)
                 suite.WriteToConsole();
-
             
             Console.WriteLine(string.Format("Total : {0} Pass : {1} Fail {2}", PassCount + FailCount, PassCount,
                                             FailCount));
 
             Console.WriteLine("======================================================================");
         }
+
         public void WriteToStream(StreamWriter sw)
         {
-            sw.WriteLine("<testsuites>");
-            foreach (TestSuite suite in Suites)
-                suite.WriteToStream(sw);
-            sw.WriteLine("</testsuites>");
+            _testReporter.WriteTestSuiteListToStream(this, sw);
+
+            //sw.WriteLine("<testsuites>");
+            //foreach (TestSuite suite in Suites)
+            //    suite.WriteToStream(sw);
+            //sw.WriteLine("</testsuites>");
         }
     }
     class TestSuite
@@ -49,17 +60,17 @@ namespace HDWA.SeleniumTestRunner
             foreach( TestCase tc in TestCases)
                 tc.WriteToConsole();
         }
-        public void WriteToStream(StreamWriter sw)
-        {
-            sw.WriteLine(string.Format("<testsuite name=\"{0}\" tests=\"{1}\" failures=\"{2}\">",
-                Title, PassCount + FailCount, FailCount
-                ));
+        //public void WriteToStream(StreamWriter sw)
+        //{
+        //    //sw.WriteLine(string.Format("<testsuite name=\"{0}\" tests=\"{1}\" failures=\"{2}\">",
+        //    //    Title, PassCount + FailCount, FailCount
+        //    //    ));
             
-            foreach (TestCase tc in TestCases)
-                tc.WriteToStream(sw);
+        //    //foreach (TestCase tc in TestCases)
+        //    //    tc.WriteToStream(sw);
 
-            sw.WriteLine("</testsuite>");
-        }
+        //    //sw.WriteLine("</testsuite>");
+        //}
     }
 
     internal class TestCase
@@ -68,19 +79,36 @@ namespace HDWA.SeleniumTestRunner
         public string Title;
         public string ErrorMessage;
         public string PageName;
+
+        public long DurationMilliseconds;
+        private Stopwatch _stopwatch;
+
+        public TestCase()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
         public void WriteToConsole()
         {
             Console.WriteLine(string.Format("   {0}", Title));
             if (!Passed)
                 Console.WriteLine(string.Format("       {0}", ErrorMessage));
         }
-        public void WriteToStream(StreamWriter sw)
+
+        public void RecordDuration()
         {
-            sw.WriteLine(string.Format("<testcase name=\"{0}\"> classname=\"Jasmine.{1}\"", Title, PageName ));
-            if (!Passed)
-                sw.WriteLine(string.Format("<error >{0}</error>", ErrorMessage));
-            sw.WriteLine("</testcase>");
+            DurationMilliseconds = _stopwatch.ElapsedMilliseconds;
+            _stopwatch.Stop();
         }
+
+        //public void WriteToStream(StreamWriter sw)
+        //{
+        //    sw.WriteLine(string.Format("<testcase name=\"{0}\"> classname=\"Jasmine.{1}\"", Title, PageName ));
+        //    if (!Passed)
+        //        sw.WriteLine(string.Format("<error >{0}</error>", ErrorMessage));
+        //    sw.WriteLine("</testcase>");
+        //}
     }
 
     class Program
@@ -97,6 +125,8 @@ namespace HDWA.SeleniumTestRunner
                 CmdLineString inputUrlList = new CmdLineString("input-url-file", false, "Input file containing urls to test - defaults to SpecRunnerList.txt");
                 CmdLineParameter runChrome = new CmdLineParameter("chrome", false, "Run Selenium with the chrome driver");
                 CmdLineParameter runIE = new CmdLineParameter("ie", false, "Run Selenium with the ie driver");
+                CmdLineParameter timeout = new CmdLineParameter("timeout", false, "Timeout value (seconds) to wait for the tests to finish. defaults to 90.");
+                CmdLineParameter reporter = new CmdLineParameter("reporter", false, "Reporter type : jenkins | teamcity.  Defaults to teamcity reporter "); 
 
 
                 ccl.RegisterParameter(outputFileName);
@@ -105,6 +135,8 @@ namespace HDWA.SeleniumTestRunner
                 ccl.RegisterParameter(inputUrlList);
                 ccl.RegisterParameter(runChrome);
                 ccl.RegisterParameter(runIE);
+                ccl.RegisterParameter(timeout);
+                ccl.RegisterParameter(reporter);
                 ccl.Parse(args);
 
 
@@ -112,31 +144,15 @@ namespace HDWA.SeleniumTestRunner
                 string strChromeBaseDir = !string.IsNullOrEmpty(chromeBaseDir.Value) ? chromeBaseDir.Value : @"E:\source\github\selenium-jasmine-runner\chromedriver_win32_2.2";
                 string strIEBaseDir = !string.IsNullOrEmpty(ieBaseDir.Value) ? ieBaseDir.Value : @"E:\source\github\selenium-jasmine-runner\IEDriverServer_x64_2.34.0";
                 string strSpecRunnerListFile = !string.IsNullOrEmpty(inputUrlList.Value) ? inputUrlList.Value : "SpecRunnerList.txt";
+                string strReporter = !string.IsNullOrEmpty(reporter.Value) ? reporter.Value : "teamcity";
 
-                //switch (args.Length)
-                //{
-                //    case 0:
-                //        Console.WriteLine(
-                //            "Please specify a text file as the first argument. this file should contain all specs to run i.e. 'http://localhost:5055/SpecRunner_JourneyBoard.html'");
-                //        Console.WriteLine("Please specify the path to chrome base dir as the 2nd argument i.e. " +
-                //                          @"C:\source\CWB\PatientFlowTrunk\SharedBinaries\selenium\chromedriver_win_26.0.1383.0");
-                //        Console.WriteLine("Output path third arg.");
-                //        break;
-                //    case 1:
-                //        strSpecRunnerListFile = args[0];
-                //        break;
-                //    case 2:
-                //        strSpecRunnerListFile = args[0];
-                //        strChromeBaseDir = args[1];
-                //        break;
-                //    case 3:
-                //        strSpecRunnerListFile = args[0];
-                //        strChromeBaseDir = args[1];
-                //        strOutputFileName = args[2];
-                //        break;
-                //}
+                short timeoutValue = 90;
+                if (!string.IsNullOrEmpty(timeout.Value))
+                {
+                    Int16.TryParse(timeout.Value, out timeoutValue);
+                }
 
-                TestSuites testSuites = new TestSuites();
+                TestSuites testSuites = new TestSuites(TestReporterFactory.GetTestReporter(strReporter));
 
                 List<string> strFileList = new List<string> ();
                 using (FileStream fs = new FileStream(strSpecRunnerListFile , FileMode.Open, FileAccess.Read))
@@ -153,23 +169,6 @@ namespace HDWA.SeleniumTestRunner
                     
                 }
 
-                if (runIE.Exists)
-                {
-                    using (var driver = new OpenQA.Selenium.IE.InternetExplorerDriver(strIEBaseDir))
-
-                    {
-                        foreach (string strSpecRunner in strFileList)
-                        {
-                            string strPageName = strSpecRunner.Substring(strSpecRunner.LastIndexOf('/') + 1);
-                            if (strPageName.IndexOf('.') > 0)
-                                strPageName = strPageName.Substring(0, strPageName.IndexOf('.'));
-
-                            RunAllJasmineTestsAndReportTrivialReporter(driver, strSpecRunner, strPageName, "IE", ref testSuites);
-                        }
-
-                    }
-                }
-
                 if (runChrome.Exists)
                 {
                     using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver(strChromeBaseDir))
@@ -180,11 +179,37 @@ namespace HDWA.SeleniumTestRunner
                             if (strPageName.IndexOf('.') > 0)
                                 strPageName = strPageName.Substring(0, strPageName.IndexOf('.'));
 
-                            RunAllJasmineTestsAndReportTrivialReporter(driver, strSpecRunner, strPageName, "Chrome", ref testSuites);
+                            RunAllJasmineTestsAndReportTrivialReporter(driver, timeoutValue, strSpecRunner, strPageName, "Chrome", ref testSuites);
                         }
 
                     }
                 }
+
+                if (runIE.Exists)
+                {
+
+                    InternetExplorerOptions options = new InternetExplorerOptions();
+                    options.EnableNativeEvents = false;
+                    options.EnsureCleanSession = true;
+                    
+                    //IWebDriver driver = new InternetExplorerDriver(options);
+
+                    using (var driver = new OpenQA.Selenium.IE.InternetExplorerDriver(strIEBaseDir))
+
+                    {
+                        foreach (string strSpecRunner in strFileList)
+                        {
+                            string strPageName = strSpecRunner.Substring(strSpecRunner.LastIndexOf('/') + 1);
+                            if (strPageName.IndexOf('.') > 0)
+                                strPageName = strPageName.Substring(0, strPageName.IndexOf('.'));
+
+                            RunAllJasmineTestsAndReportTrivialReporter(driver, timeoutValue, strSpecRunner, strPageName, "IE", ref testSuites);
+                        }
+
+                    }
+                }
+
+               
 
                 Console.WriteLine("-----------");
 
@@ -198,6 +223,13 @@ namespace HDWA.SeleniumTestRunner
 
                     streamWriter.Flush();
                 }
+
+                using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
+                {
+                    sw.AutoFlush = true;
+                    testSuites.WriteToStream(sw);
+                    sw.Flush();
+                }
             }
             catch (Exception ex)
             {
@@ -205,18 +237,18 @@ namespace HDWA.SeleniumTestRunner
                 Console.WriteLine("Unhandled Exception " + ex.ToString());
                 Console.WriteLine("");
                 Console.WriteLine(ex.ToString());
+                throw ex;
             }
 
 
         }
 
-
-        private static void RunAllJasmineTestsAndReportTrivialReporter(IWebDriver driver, string TEST_PAGE, string strPageName, string browser, ref TestSuites testSuites )
+        private static void RunAllJasmineTestsAndReportTrivialReporter(IWebDriver driver, short timeoutValue, string TEST_PAGE, string strPageName, string browser, ref TestSuites testSuites )
         {
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(90));
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(timeoutValue));
             driver.Navigate().GoToUrl(TEST_PAGE);
 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(90));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutValue));
 
             bool bFound = wait.Until(d => { return 
                 (d.FindElement(By.CssSelector("span.finished-at")).Text.Length > 0); 
@@ -224,13 +256,25 @@ namespace HDWA.SeleniumTestRunner
 
             string duration = driver.FindElement(By.CssSelector("span.finished-at")).Text;
 
-            IWebElement showPassed = driver.FindElement(By.Id("__jasmine_TrivialReporter_showPassed__"));
-            if (!showPassed.Selected)
-                showPassed.Click();
+            //IWebElement showPassed = driver.FindElement(By.Id("__jasmine_TrivialReporter_showPassed__"));
+            if (!driver.FindElement(By.Id("__jasmine_TrivialReporter_showPassed__")).Selected)
+            {
+                IJavaScriptExecutor executor = driver as IJavaScriptExecutor;
+                executor.ExecuteScript("document.getElementById('__jasmine_TrivialReporter_showPassed__').click()");
+                // note this code does not work on IE !!
+                //showPassed.Click(); 
+            }
 
-            Thread.Sleep(3000);
+            Thread.Sleep(1000);
 
-            var suites = driver.FindElements(By.CssSelector("div.suite"));
+            
+            var suites = driver.FindElements(By.CssSelector("div#TrivialReporter.jasmine_reporter > div.suite"));
+
+            //foreach (var element in suites)
+            //{
+            //    var topLevelSuites = element.FindElements(By.CssSelector("div.suite"));
+            //}
+
             foreach (var suite in suites)
             {
                 // suite pass / fail
@@ -265,6 +309,8 @@ namespace HDWA.SeleniumTestRunner
                         var resultMessage = spec.FindElement(By.CssSelector("div.resultMessage"));
                         testCase.ErrorMessage = resultMessage.Text;
                     }
+
+                    testCase.RecordDuration();
 
                     testSuite.TestCases.Add(testCase);
                 }
---
+++ TestReporters.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using HDWA.SeleniumTestRunner;
+
+namespace SeleniumJasmineRunner
+{
+    internal interface ITestReporter
+    {
+        void WriteTestCaseToStream(TestCase testCase, StreamWriter sw, TestSuite testSuite);
+        void WriteTestSuiteToStream(TestSuite testCase, StreamWriter sw);
+        void WriteTestSuiteListToStream(TestSuites testCase, StreamWriter sw);
+    }
+
+    class TestReporterFactory
+    {
+        public static ITestReporter GetTestReporter(string reporter)
+        {
+            switch (reporter)
+            {
+                case "jenkins":
+                    return new JenkinsReporter();
+            }
+            return new TeamCityReporter();
+        }
+    }
+
+    internal class JenkinsReporter : ITestReporter
+    {
+        public void WriteTestCaseToStream(TestCase testCase, StreamWriter sw, TestSuite testSuite)
+        {
+            sw.WriteLine(string.Format("<testcase name=\"{0}\"> classname=\"Jasmine.{1}\"", testCase.Title, testCase.PageName));
+            if (!testCase.Passed)
+                sw.WriteLine(string.Format("<error >{0}</error>", testCase.ErrorMessage));
+            sw.WriteLine("</testcase>");
+        }
+
+        public void WriteTestSuiteToStream(TestSuite testSuite, StreamWriter sw)
+        {
+            sw.WriteLine(string.Format("<testsuite name=\"{0}\" tests=\"{1}\" failures=\"{2}\">",
+                testSuite.Title, testSuite.PassCount + testSuite.FailCount, testSuite.FailCount
+                ));
+
+            foreach (TestCase tc in testSuite.TestCases)
+                WriteTestCaseToStream(tc, sw, testSuite);
+
+            sw.WriteLine("</testsuite>");
+        }
+
+
+        public void WriteTestSuiteListToStream(TestSuites testSuiteList, StreamWriter sw)
+        {
+            sw.WriteLine("<testsuites>");
+            foreach (TestSuite suite in testSuiteList.Suites)
+                WriteTestSuiteToStream(suite, sw);
+            sw.WriteLine("</testsuites>");
+        }
+    }
+
+    internal class TeamCityReporter : ITestReporter
+    {
+        public void WriteTestCaseToStream(TestCase testCase, StreamWriter sw, TestSuite testSuite)
+        {
+            string fulltestName = testSuite.Title + " => " + testCase.Title;
+
+            sw.WriteLine(string.Format("##teamcity[testStarted name='{0}' captureStandardOutput='true']", fulltestName));
+            if (!testCase.Passed)
+                sw.WriteLine(string.Format("##teamcity[testFailed name='{0}' message='{1}']", fulltestName, testCase.ErrorMessage.Replace('\'', '"')));
+            sw.WriteLine(string.Format("##teamcity[testFinished name='{0}' duration='{1}']", fulltestName, testCase.DurationMilliseconds));
+        }
+
+        public void WriteTestSuiteToStream(TestSuite testSuite, StreamWriter sw)
+        {
+            sw.WriteLine(string.Format("##teamcity[testSuiteStarted name='{0}']", testSuite.Title//, testSuite.PassCount + testSuite.FailCount, testSuite.FailCount
+                ));
+
+            foreach (TestCase tc in testSuite.TestCases)
+                WriteTestCaseToStream(tc, sw, testSuite);
+
+            sw.WriteLine(string.Format("##teamcity[testSuiteFinished name='{0}']", testSuite.Title//, testSuite.PassCount + testSuite.FailCount, testSuite.FailCount
+                ));
+        }
+
+        public void WriteTestSuiteListToStream(TestSuites testSuiteList, StreamWriter sw)
+        {
+            //sw.WriteLine("<testsuites>");
+            foreach (TestSuite suite in testSuiteList.Suites)
+                WriteTestSuiteToStream(suite, sw);
+            //sw.WriteLine("</testsuites>");
+        }
+    }
+
+
+}
